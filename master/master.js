50a5f07105975a56b96c25faa23eef94889cd2e6
arky0925 <arky.25242918@icloud.com>
2025-01-11 01:16:05 +0900

    Add files via upload

---
+++ master/master.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // 各ボタンのクリックイベントを設定
+    document.getElementById('homeButton').addEventListener('click', function() {
+        window.location.href = 'index.html'; // 遷移先のページ
+    });
+
+    document.getElementById('couponButton').addEventListener('click', function() {
+        window.location.href = 'WAB.html'; // 遷移先のページ
+    });
+
+    document.getElementById('memoryButton').addEventListener('click', function() {
+        window.location.href = 'WAC.html'; // 遷移先のページ
+    });
+
+    document.getElementById('stampButton').addEventListener('click', function() {
+        window.location.href = 'WAD.html'; // 遷移先のページ
+    });
+
+    document.getElementById('knowledgeButton').addEventListener('click', function() {
+        window.location.href = 'WAE.html'; // 遷移先のページ
+    });
+});
