b87efd11b9ca48844e36af6ff9e62ed843652708
arky0925 <arky.25242918@icloud.com>
2025-02-03 22:22:00 +0900

    Update WAC.js

--- js/WAC.js
+++ js/WAC.js
@@ -1315,6 +1315,12 @@ document.addEventListener('DOMContentLoaded', () => {
 				input.focus(); // inputにフォーカスを戻す
 			}
 		});
+		// フォーカス時にスクロールを無効にする
+		input.addEventListener('focus', function() {
+			window.addEventListener('scroll', function (event) {
+				event.preventDefault();
+			}, { passive: false });
+		});
 	});
 
 	// 確認のための入力ボックス
